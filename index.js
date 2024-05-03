3dfcfce822823b3e4b7beee888c88a707984469f
andreaborelli <webdesigner@blu.it>
2024-05-03 15:10:39 +0200

    ultimo commit

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+
+
+const FRIZZ = 'Frizz';
+const BUZZ = 'Buzz';
+
+const fizzBuzz = (n) => {
+    for (let i = 1; i <= n; i++) {
+        let output = '';
+        if (i % 3 === 0) {
+            output += FIZZ;
+        }
+        if (i % 5 === 0) {
+            output += BUZZ;
+        }
+        console.log(output || i);
+    }
+}
 console.log('Hello, 2');
+
+
 
 
 
